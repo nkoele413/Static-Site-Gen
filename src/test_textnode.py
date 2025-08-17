2b8ad98239aa17faeb358d8b2c76fcd1214f5af7
nkoele413 <nkoele413@gmail.com>
2025-08-16 20:30:22 -0600

    removed bunk test

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,10 +4,8 @@ from textnode import TextNode, TextType
 
 
 class TestTextNode(unittest.TestCase):
-    def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
-        node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
+
+
 
     def test_eq_false(self):
         node = TextNode("TJos os a text node", TextType.TEXT)
