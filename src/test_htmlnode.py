fb3dc847fd2322efba65034425ce1da2d57e6945
nkoele413 <nkoele413@gmail.com>
2025-08-16 21:08:41 -0600

    leafNode class added

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
@@ -36,3 +36,18 @@ class TestHTMLNode(unittest.TestCase):
             node.props,
             None,
         )
+
+    def test_left_to_html(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
