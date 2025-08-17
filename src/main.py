1620979709bc1995fc71e65ad2678d2c5a0858db
nkoele413 <nkoele413@gmail.com>
2025-08-16 20:24:17 -0600

    tweaking unit testing again

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
 main()
