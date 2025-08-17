eefc9a520e776c19b2724ed4c8cefdf7cba8a8f2
nkoele413 <nkoele413@gmail.com>
2025-08-16 21:31:08 -0600

    fixed typo

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -39,7 +39,7 @@ class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
 
-    def tp_html(self):
+    def to_html(self):
         if self.tag is None:
             raise ValueError("invalid HTML: no tag")
         if self.children is None:
