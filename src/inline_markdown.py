7b5f2ce3d078f25ba9486af16dc7497ba78e9bb4
nkoele413 <nkoele413@gmail.com>
2025-08-17 18:13:52 -0600

    reworked inline testing

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,13 +1,14 @@
 from textnode import TextNode, TextType
 
-def split_nodes_delimeter(old_nodes, delimeter, text_type):
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
         split_nodes = []
-        sections = old_node.text.split(delimeter)
+        sections = old_node.text.split(delimiter)
         if len(sections) % 2 == 0:
             raise ValueError("invalid markdown, formatted section not closed")
         for i in range(len(sections)):
