9c65b7977a25bde5b30647f82f00e0d10099a2fb
nkoele413 <nkoele413@gmail.com>
2025-08-17 18:20:42 -0600

    fix

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -49,6 +49,17 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_delim_italic(self):
+        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
 
     def test_delim_bold_and_italic(self):
         node = TextNode("**bold** and _italic_", TextType.TEXT)
