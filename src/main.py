1d31953047756f8928b3e851bbd982e9ef887274
nkoele413 <nkoele413@gmail.com>
2025-08-18 16:08:36 -0600

    fix attempt

--- src/main.py
+++ src/main.py
@@ -3,15 +3,18 @@ import shutil
 
 from copystatic import copy_files_recursive
 
+
 dir_path_static = "./static"
 dir_path_public = "./public"
+
 
 def main():
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static filers to public directory...")
+    print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
 
 main()
