e1dd71cd5fdd2316a4189a73d0f02e2a3ce4fa82
nkoele413 <nkoele413@gmail.com>
2025-08-17 18:08:45 -0600

    testing change

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -47,19 +47,6 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
     
-    def test_delim_bold_and_italic(self):
-        node = TextNode("**bold** and _italic_", TextType.TEXT)
-        new_nodes = split_nodes_delimeter([node], "**", TextType.BOLD)
-        new_nodes = split_nodes_delimeter(new_nodes, "_", TextType.ITALIC)
-        self.assertEqual(
-            [
-                TextNode("bold", TextType.BOLD),
-                TextNode(" and ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC),
-            ],
-            new_nodes,
-        )
-    
     def test_delim_code(self):
         node = TextNode("This is text with a 'code block' word", TextType.TEXT)
         new_nodes = split_nodes_delimeter([node], "'", TextType.CODE)
