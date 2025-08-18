7fa0b3b838836ed1d9dad78916efa4ca3283edc0
nkoele413 <nkoele413@gmail.com>
2025-08-17 18:22:29 -0600

    fix

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -7,84 +7,30 @@ from textnode import TextNode, TextType
 
 
 class TestInlineMarkdown(unittest.TestCase):
-    def test_delim_bold(self):
-        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertListEqual(
-            [
-                TextNode("This is text with a ", TextType.TEXT),
-                TextNode("bolded", TextType.BOLD),
-                TextNode(" word", TextType.TEXT),
-            ],
-            new_nodes,
-        )
+    def test_split_nodes_delimiter(self):
+        old_nodes = [
+            TextNode("This is a **bold** text", TextType.TEXT),
+            TextNode("This is an *italic* text", TextType.TEXT),
+        ]
+        delimiter = "**"
+        text_type = TextType.BOLD
 
-    def test_delim_bold_double(self):
-        node = TextNode(
-            "This is text with a **bolded** word and **another**", TextType.TEXT
-        )
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertListEqual(
-            [
-                TextNode("This is text with a ", TextType.TEXT),
-                TextNode("bolded", TextType.BOLD),
-                TextNode(" word and ", TextType.TEXT),
-                TextNode("another", TextType.BOLD),
-            ],
-            new_nodes,
-        )
+        new_nodes = split_nodes_delimiter(old_nodes, delimiter, text_type)
 
-    def test_delim_bold_multiword(self):
-        node = TextNode(
-            "This is text with a **bolded word** and **another**", TextType.TEXT
-        )
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertListEqual(
-            [
-                TextNode("This is text with a ", TextType.TEXT),
-                TextNode("bolded word", TextType.BOLD),
-                TextNode(" and ", TextType.TEXT),
-                TextNode("another", TextType.BOLD),
-            ],
-            new_nodes,
-        )
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0].text, "This is a ")
+        self.assertEqual(new_nodes[1].text, "bold")
+        self.assertEqual(new_nodes[2].text, " text")
 
-    def test_delim_italic(self):
-        node = TextNode("This is text with an _italic_ word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
-        self.assertListEqual(
-            [
-                TextNode("This is text with an ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC),
-                TextNode(" word", TextType.TEXT),
-            ],
-            new_nodes,
-        )
+    def test_split_nodes_invalid_markdown(self):
+        old_nodes = [TextNode("This is an **unclosed bold text", TextType.TEXT)]
+        delimiter = "**"
+        text_type = TextType.BOLD
 
-    def test_delim_bold_and_italic(self):
-        node = TextNode("**bold** and _italic_", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
-        self.assertListEqual(
-            [
-                TextNode("bold", TextType.BOLD),
-                TextNode(" and ", TextType.TEXT),
-                TextNode("italic", TextType.ITALIC),
-            ],
-            new_nodes,
-        )
-
-    def test_delim_code(self):
-        node = TextNode("This is text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertListEqual(
-            [
-                TextNode("This is text with a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" word", TextType.TEXT),
-            ],
-            new_nodes,
-        )
+        with self.assertRaises(ValueError) as context:
+            split_nodes_delimiter(old_nodes, delimiter, text_type)
+        
+        self.assertTrue("invalid markdown, formatted section not closed" in str(context.exception))
 
 
 if __name__ == "__main__":
